fcd67b5148f32a0bd41897c5ac6f6442ba79875d
Antoine Meloche <antoine.meloche@polymtl.ca>
2024-10-04 16:03:32 -0400

    separate ground-station and cubesat compilation targets

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
+#[cfg(ground_station)]
 use pyo3::prelude::*;
 mod pack;
 
 mod utils;
 
 /// Formats the sum of two numbers as string.
+#[cfg(ground_station)]
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
 /// A Python module implemented in Rust.
+#[cfg(ground_station)]
 #[pymodule]
 fn comms(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
